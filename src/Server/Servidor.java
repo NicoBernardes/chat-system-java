0da25c7eb8014b5a3e668611dddce45cfdfae1c4
Nicolas <nicolasbernardes84@gmail.com>
2022-04-19 21:20:40 -0300

    Project Created

---
+++ src/Server/Servidor.java
@@ -0,0 +1,38 @@
+package Server;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class Servidor extends Thread {
+
+	private static ArrayList<BufferedWriter> clientes;
+	private static ServerSocket server;
+	private String nome;
+	private Socket con;
+	private InputStream in;
+	private InputStreamReader inr;
+	private BufferedReader bfr;
+
+	public Servidor(Socket con) {
+		this.con = con;
+		try {
+			in = con.getInputStream();
+			inr = new InputStreamReader(in);
+			bfr = new BufferedReader(inr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
